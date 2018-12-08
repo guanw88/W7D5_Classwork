2108aea3548b6c7137798d026b2074bf718b1406
guanw88 <guanw88@gmail.com>
2018-12-07 16:20:10 -0800

    Complete reducers and front end auth

---
+++ bench-bnb/frontend/actions/session_actions.js
@@ -0,0 +1,53 @@
+import * as SessionApiUtil from "../util/session_api_util";
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+
+export const receiveCurrentUser = (currentUser) => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+  };
+};
+
+export const logoutCurrentUser = () => {
+  return {
+    type: LOGOUT_CURRENT_USER
+  };
+};
+
+export const receiveErrors = (errors) => {
+  return {
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+  };
+};
+
+const dispatchErrors = (dispatch, xhr) => {
+  dispatch (receiveErrors(xhr.responseJSON));
+};
+
+export const login = user => dispatch => {
+  return SessionApiUtil.login(user)
+    .then(
+      user => dispatch( receiveCurrentUser(user) ),
+      xhr => dispatchErrors(dispatch, xhr)
+    );
+};
+
+export const logout = () => dispatch => {
+  return SessionApiUtil.logout()
+    .then(
+      () => dispatch( logoutCurrentUser() ),
+      xhr => dispatchErrors(dispatch, xhr)
+    );
+};
+
+export const signup = user => dispatch => {
+  return SessionApiUtil.signup(user)
+    .then(
+      user => dispatch( receiveCurrentUser(user) ) ,
+      xhr => dispatchErrors(dispatch, xhr)
+    );
+};
--- bench-bnb/frontend/bench_bnb.jsx
+++ bench-bnb/frontend/bench_bnb.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { signup, login, logout } from "./util/session_api_util";
-
+import * as SessionActions from "./actions/session_actions";
+import configureStore from "./store/store";
 
 document.addEventListener('DOMContentLoaded', () => {
-  testApiUtil();
+  testActions();
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
@@ -13,4 +17,13 @@ const testApiUtil = () => {
   window.signup = signup;
   window.login = login;
   window.logout = logout;
+};
+
+const testActions = () => {
+  window.receiveCurrentUser = SessionActions.receiveCurrentUser;
+  window.logoutCurrentUser = SessionActions.logoutCurrentUser;
+  window.receiveErrors = SessionActions.receiveErrors;
+  window.login = SessionActions.login;
+  window.logout = SessionActions.logout;
+  window.signup = SessionActions.signup;
 };
