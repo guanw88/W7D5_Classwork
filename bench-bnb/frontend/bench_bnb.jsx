2dccb6f6d0be13d00841999b9d62915a34c283b3
gwang <guanw88@gmail.com>
2018-12-10 11:26:06 -0800

    Bootstrap current user from backend and use it to define preloadedState

--- bench-bnb/frontend/bench_bnb.jsx
+++ bench-bnb/frontend/bench_bnb.jsx
@@ -7,7 +7,19 @@ import configureStore from "./store/store";
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+  } else {
+    store = configureStore();
+  }
+
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   testActions();
